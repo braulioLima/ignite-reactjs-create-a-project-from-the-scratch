f22f48d79d3f111c43f5aa37ef0a5476773b20d0
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2021-12-01 20:47:32 -0300

    chore:add a font and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,29 @@
-import Document from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="/favicon.png" type="image/png" />
+
+          <meta
+            name="description"
+            content="Um blog muito foda sobre o melhor da tecnologia."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </>
+    );
+  }
 }
