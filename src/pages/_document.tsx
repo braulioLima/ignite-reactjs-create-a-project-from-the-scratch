9e7ba7cdc3e5dde7cfcb46f4ec8ff7272e37cc6d
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2021-12-03 17:19:54 -0300

    fix:add Html missing tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,26 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
+        <Html>
+          <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+              rel="stylesheet"
+            />
 
-          <link rel="icon" href="/favicon.png" type="image/png" />
+            <link rel="icon" href="/favicon.png" type="image/png" />
 
-          <meta
-            name="description"
-            content="Um blog muito foda sobre o melhor da tecnologia."
-          />
-        </Head>
+            <meta
+              name="description"
+              content="Um blog muito foda sobre o melhor da tecnologia."
+            />
+          </Head>
+        </Html>
         <body>
           <Main />
           <NextScript />
